db5903b2861b2e4df4a02acf478298b7207b653b
borisdanev <borisdanev9@gmail.com>
2024-01-16 01:06:23 +0100

    Implemented login functionality

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import AuthOverlay from "./AuthOverlay";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import * as Yup from "yup";
+import { useGetUserQuery } from "../store";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "../store";
 interface Props {
   setOpenedForm: Function;
 }
@@ -11,24 +16,40 @@ interface FormValues {
   password: string;
 }
 const LoginForm: React.FC<Props> = ({ setOpenedForm }) => {
+  const dispatch = useDispatch();
   const initialValues: FormValues = {
     email: "",
     password: "",
   };
   const validationSchema = Yup.object().shape({
-    email: Yup.string().required("Email is required"),
-    // .test(
-    //   "email",
-    //   "Email is alredy in use",
-    //   (value) => !emailsList?.includes(value)
-    // ),
-    password: Yup.string().required("Password is required"),
+    email: Yup.string()
+      .required("Email is required")
+      .test("email", "Incorrect email or password", (value) => {
+        if (data) return true;
+        return false;
+      }),
+    password: Yup.string()
+      .required("Password is required")
+      .test("password", "Incorrect email or password", (value) => {
+        if (value === data?.password) return true;
+        return false;
+      }),
   });
-
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    if (data) dispatch(setCurrentUser(data));
+    setOpenedForm(false);
+  };
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleSubmit,
+    validateOnChange: false,
+    validateOnBlur: false,
+  });
+  const { data } = useGetUserQuery(formik.values.email);
   return (
     <AuthOverlay setOpenedForm={setOpenedForm}>
-      <form className="auth-form" onClick={handleSubmit}>
+      <form className="auth-form" onClick={formik.handleSubmit}>
         <Box
           sx={{
             my: 3,
@@ -38,13 +59,24 @@ const LoginForm: React.FC<Props> = ({ setOpenedForm }) => {
             width: "100%",
           }}
         >
-          {["Email", "Password"].map((item, i) => (
+          {[
+            { text: "Email", value: "email" },
+            { text: "Password", value: "password" },
+          ].map((item, i) => (
             <TextField
               key={i}
+              id={item.value}
+              name={item.value}
               variant="outlined"
-              label={item}
+              label={item.text}
               sx={{ my: i === 1 ? 2 : 0 }}
+              helperText={
+                formik.touched[item.value as keyof FormValues] &&
+                formik.errors[item.value as keyof FormValues]
+              }
               className="form-input"
+              value={formik.values[item.value as keyof FormValues]}
+              onChange={formik.handleChange}
             />
           ))}
         </Box>
