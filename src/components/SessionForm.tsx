2bf5b61e0685b022f0ca9f5be34d0b1c1f627305
borisdanev <borisdanev9@gmail.com>
2024-01-29 17:16:10 +0100

    Added UI for selecting exercises for training session

--- src/components/SessionForm.tsx
+++ src/components/SessionForm.tsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
+import { useGetExercisesQuery } from "../store";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 import FormContainer from "./FormContainer";
 import SelectionCalendar from "./SelectionCalendar";
+import { CiSquarePlus } from "react-icons/ci";
+import { ExerciseModel } from "../types/exercise.model";
 const SessionForm: React.FC = () => {
   const [selectedDays, setSelectedDays] = useState<string[]>([]);
+  const [selectedExercises, setSelectedExercises] = useState<ExerciseModel[]>(
+    []
+  );
+  const [visibleExerciseSelection, setVisibleExerciseSelection] =
+    useState<boolean>(false);
+  const { data: exercises } = useGetExercisesQuery();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement> | undefined) => {};
   return (
     <FormContainer text="Sessions" handleSubmit={handleSubmit}>
@@ -10,6 +22,24 @@ const SessionForm: React.FC = () => {
         selectedDays={selectedDays}
         setSelectedDays={setSelectedDays}
       />
+      {selectedDays.length > 0 ? (
+        <Box>
+          <TextField label="Session Name" />
+          {selectedExercises.map((item, i) => (
+            <Typography>{i}</Typography>
+          ))}
+          <Box
+            sx={{ display: "flex", alignItems: "center" }}
+            onClick={() => setVisibleExerciseSelection(true)}
+          >
+            <CiSquarePlus fontSize="2rem" />
+            <Typography sx={{ ml: 1 }}>Add Exercise</Typography>
+          </Box>
+          {visibleExerciseSelection ? <Box>Something</Box> : <Box></Box>}
+        </Box>
+      ) : (
+        <Typography></Typography>
+      )}
     </FormContainer>
   );
 };
