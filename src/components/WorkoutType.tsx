de78198f8a02a9b4397a1cc0aef4d8a6aac92a84
borisdanev <borisdanev9@gmail.com>
2024-03-05 15:26:05 +0100

    Implemented responsivness for WorkoutTypesList component

---
+++ src/components/WorkoutType.tsx
@@ -0,0 +1,52 @@
+import { useDispatch } from "react-redux";
+import { setWorkoutType } from "../store";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import { WorkoutType } from "../enums/WorkoutType";
+interface Props {
+  src: string;
+  text: string;
+  type: WorkoutType;
+}
+const WorkoutTypeItem: React.FC<Props> = ({ src, text, type }) => {
+  const dispatch = useDispatch();
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "25rem",
+        borderTopLeftRadius: "50%",
+        borderTopRightRadius: "50%",
+        borderBottomLeftRadius: "1rem",
+        borderBottomRightRadius: "1rem",
+        bgcolor: "#37423d",
+      }}
+    >
+      <img
+        src={src}
+        style={{
+          width: "100%",
+          height: "18.81rem",
+          // aspectRatio: "1",
+          objectFit: "cover",
+          borderRadius: "50%",
+        }}
+        alt={text}
+      />
+      <Typography className="h3 " textAlign="center" sx={{}}>
+        {text}
+      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "end", mt: 2, pr: 1 }}>
+        <Link
+          href="#suggestions"
+          underline="none"
+          onClick={() => dispatch(setWorkoutType(type))}
+        >
+          FIND WORKOUTS
+        </Link>
+      </Box>
+    </Box>
+  );
+};
+export default WorkoutTypeItem;
--- src/components/WorkoutTypesList.tsx
+++ src/components/WorkoutTypesList.tsx
@@ -1,86 +1,75 @@
-import { useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
-import { setWorkoutType } from "../store";
+import useScreenSize from "../hooks/useScreenSize";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
 import MuslceBuildingType from "../images/muscle_building_type.webp";
 import StrenghType from "../images/strength_type.webp";
 import WeightLossType from "../images/weight_loss_type.webp";
 import EnduranceType from "../images/endurance_type.webp";
 import { WorkoutType } from "../enums/WorkoutType";
+import Slider from "react-slick";
+import WorkoutTypeItem from "./WorkoutType";
+const sliderSettings = {
+  arrows: true,
+  dots: false,
+  swipe: true,
+  slidesToShow: 2,
+};
 const WorkoutTypesList: React.FC = () => {
-  const dispatch = useDispatch();
+  const screenSize = useScreenSize();
+  const workoutTypes = [
+    {
+      text: "Muscle Building",
+      src: MuslceBuildingType,
+      type: WorkoutType.MuscleBuilding,
+    },
+    {
+      text: "Strength",
+      src: StrenghType,
+      type: WorkoutType.StrengthWorkout,
+    },
+    {
+      text: "Weight Loss",
+      src: WeightLossType,
+      type: WorkoutType.WeightLoss,
+    },
+    {
+      text: "Endurance",
+      src: EnduranceType,
+      type: WorkoutType.EnduranceWorkout,
+    },
+  ];
   return (
     <Box sx={{ my: 5 }}>
       <Typography className="h2 heading-color" sx={{ mb: 2 }}>
         Workout Types
       </Typography>
-      <Grid container columnSpacing={2} rowSpacing={2}>
-        {[
-          {
-            text: "Muscle Building",
-            src: MuslceBuildingType,
-            type: WorkoutType.MuscleBuilding,
-          },
-          {
-            text: "Strength",
-            src: StrenghType,
-            type: WorkoutType.StrengthWorkout,
-          },
-          {
-            text: "Weight Loss",
-            src: WeightLossType,
-            type: WorkoutType.WeightLoss,
-          },
-          {
-            text: "Endurance",
-            src: EnduranceType,
-            type: WorkoutType.EnduranceWorkout,
-          },
-        ].map((item, i) => (
-          <Grid key={i} item xs={12} md={6} lg={3}>
-            <Box
-              sx={{
-                width: "100%",
-                height: "25rem",
-                borderTopLeftRadius: "50%",
-                borderTopRightRadius: "50%",
-                borderBottomLeftRadius: "1rem",
-                borderBottomRightRadius: "1rem",
-                bgcolor: "#37423d",
-              }}
-            >
-              <img
+      {screenSize > 1300 ? (
+        <Grid container columnSpacing={2} rowSpacing={2}>
+          {workoutTypes.map((item, i) => (
+            <Grid key={i} item xs={12} md={6} lg={3}>
+              <WorkoutTypeItem
                 src={item.src}
-                style={{
-                  width: "100%",
-                  height: "18.81rem",
-                  // aspectRatio: "1",
-                  objectFit: "cover",
-                  borderRadius: "50%",
-                }}
-                alt={item.text}
+                text={item.text}
+                type={item.type}
               />
-              <Typography className="h3 " textAlign="center" sx={{}}>
-                {item.text}
-              </Typography>
-              <Box
-                sx={{ display: "flex", justifyContent: "end", mt: 2, pr: 1 }}
-              >
-                <Link
-                  href="#suggestions"
-                  underline="none"
-                  onClick={() => dispatch(setWorkoutType(item.type))}
-                >
-                  FIND WORKOUTS
-                </Link>
-              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Slider {...sliderSettings}>
+          {workoutTypes.map((item, i) => (
+            <Box key={i} sx={{ pr: 2 }}>
+              <WorkoutTypeItem
+                src={item.src}
+                text={item.text}
+                type={item.type}
+              />
             </Box>
-          </Grid>
-        ))}
-      </Grid>
+          ))}
+        </Slider>
+      )}
     </Box>
   );
 };
